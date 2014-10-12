e081181bc901cd9cf000b2ca073669cbcacc5d30
mh <mh@immerda.ch>
2014-10-12 12:00:20 +0200

    minor improvement

--- bin/local_apply.sh
+++ bin/local_apply.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-test -x /usr/local/bin/motd_gen.sh || puppet apply --hiera_config=/etc/puppet/ibox/hieradata/config.yaml --modulepath=/etc/puppet/ibox/modules/public/:/etc/puppet/ibox/modules/ibox/ -e 'include motd::puppetmaster'
+[ -x /usr/local/bin/motd_gen.sh ] || puppet apply --hiera_config=/etc/puppet/ibox/hieradata/config.yaml --modulepath=/etc/puppet/ibox/modules/public/:/etc/puppet/ibox/modules/ibox/ -e 'include motd::puppetmaster'
 puppet resource package rubygem-sqlite3 ensure=installed > /dev/null
 puppet resource package activerecord ensure=3.2.19 provider=gem > /dev/null
+
 puppet apply --show_diff --hiera_config=/etc/puppet/ibox/hieradata/config.yaml --modulepath=/etc/puppet/ibox/modules/public/:/etc/puppet/ibox/modules/ibox/ --storeconfigs --thin_storeconfigs --dbadapter=sqlite3 --dblocation=/etc/puppet/ibox/storeconfigs.sqlite3 --dbmigrate /etc/puppet/ibox/manifests/site.pp
