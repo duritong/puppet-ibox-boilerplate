84783b75cc0be7b19be2f47376f980fa32c84c13
mh <mh@immerda.ch>
2014-10-17 18:43:54 +0200

    introduce ha firewall

--- bin/local_apply.sh
+++ bin/local_apply.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-[ -x /usr/local/bin/motd_gen.sh ] || puppet apply --hiera_config=/etc/puppet/ibox/hieradata/config.yaml --modulepath=/etc/puppet/ibox/modules/public/:/etc/puppet/ibox/modules/ibox/ -e 'include motd::puppetmaster'
-puppet resource package rubygem-sqlite3 ensure=installed > /dev/null
-puppet resource package activerecord ensure=3.2.19 provider=gem > /dev/null
+if [ ! -x /usr/local/bin/motd_gen.sh ]; then
+  puppet apply --hiera_config=/etc/puppet/ibox/hieradata/config.yaml --modulepath=/etc/puppet/ibox/modules/public/:/etc/puppet/ibox/modules/ibox/ -e 'include motd::puppetmaster' > /dev/null
+  puppet resource package rubygem-sqlite3 ensure=installed allow_virtual=false > /dev/null
+  puppet resource package activerecord ensure=3.2.19 provider=gem allow_virtual=false > /dev/null
+fi
 
 puppet apply --show_diff --hiera_config=/etc/puppet/ibox/hieradata/config.yaml --modulepath=/etc/puppet/ibox/modules/public/:/etc/puppet/ibox/modules/ibox/ --storeconfigs --thin_storeconfigs --dbadapter=sqlite3 --dblocation=/etc/puppet/ibox/storeconfigs.sqlite3 --dbmigrate /etc/puppet/ibox/manifests/site.pp
