8e4f53d75ef4cc4b6d105be18e721182c06357de
mh <mh@immerda.ch>
2015-01-24 18:03:43 +0100

    introduce trocla + log to a file

--- bin/local_apply.sh
+++ bin/local_apply.sh
@@ -5,5 +5,8 @@ if [ ! -x /usr/local/bin/motd_gen.sh ]; then
   puppet resource package rubygem-sqlite3 ensure=installed allow_virtual=false > /dev/null
   puppet resource package activerecord ensure=3.2.19 provider=gem allow_virtual=false > /dev/null
 fi
+if [ ! -f /etc/puppet/troclarc.yaml ]; then
+  puppet apply --hiera_config=/etc/puppet/ibox/hieradata/config.yaml --modulepath=/etc/puppet/ibox/modules/public/:/etc/puppet/ibox/modules/ibox/ -e 'include trocla::config' > /dev/null
+fi
 
-puppet apply --show_diff --hiera_config=/etc/puppet/ibox/hieradata/config.yaml --modulepath=/etc/puppet/ibox/modules/public/:/etc/puppet/ibox/modules/ibox/ --storeconfigs --thin_storeconfigs --dbadapter=sqlite3 --dblocation=/etc/puppet/ibox/storeconfigs.sqlite3 --dbmigrate /etc/puppet/ibox/manifests/site.pp
+puppet apply --show_diff --hiera_config=/etc/puppet/ibox/hieradata/config.yaml --modulepath=/etc/puppet/ibox/modules/public/:/etc/puppet/ibox/modules/ibox/ --storeconfigs --thin_storeconfigs --dbadapter=sqlite3 --dblocation=/etc/puppet/ibox/storeconfigs.sqlite3 --dbmigrate /etc/puppet/ibox/manifests/site.pp | tee -a /var/log/ibox.log
