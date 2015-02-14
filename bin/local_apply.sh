473960b19df29d14d3950930e7e5e0909f4100dd
mh <mh@immerda.ch>
2015-02-14 15:27:17 +0100

    pass additional arguments to puppet

--- bin/local_apply.sh
+++ bin/local_apply.sh
@@ -9,4 +9,4 @@ if [ ! -f /etc/puppet/troclarc.yaml ]; then
   puppet apply --hiera_config=/etc/puppet/ibox/hieradata/config.yaml --modulepath=/etc/puppet/ibox/modules/public/:/etc/puppet/ibox/modules/ibox/ -e 'include trocla::config' > /dev/null
 fi
 
-puppet apply --show_diff --hiera_config=/etc/puppet/ibox/hieradata/config.yaml --modulepath=/etc/puppet/ibox/modules/public/:/etc/puppet/ibox/modules/ibox/ --storeconfigs --thin_storeconfigs --dbadapter=sqlite3 --dblocation=/etc/puppet/ibox/storeconfigs.sqlite3 --dbmigrate /etc/puppet/ibox/manifests/ | tee -a /var/log/ibox.log
+puppet apply --show_diff --hiera_config=/etc/puppet/ibox/hieradata/config.yaml --modulepath=/etc/puppet/ibox/modules/public/:/etc/puppet/ibox/modules/ibox/ --storeconfigs --thin_storeconfigs --dbadapter=sqlite3 --dblocation=/etc/puppet/ibox/storeconfigs.sqlite3 --dbmigrate /etc/puppet/ibox/manifests/ $@ | tee -a /var/log/ibox.log
